124f29bc6066872338b354ee087632da1e171911
Loren <liangliangpro@outlook.com>
2023-12-05 23:15:32 +0800

    feat: init

---
+++ Bootcamp-SwiftUI/Bootcamp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bootcamp_SwiftUIApp.swift
+//  Bootcamp-SwiftUI
+//
+//  Created by loren on 2023/12/5.
+//
+
+import SwiftUI
+
+@main
+struct Bootcamp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bootcamp-SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Bootcamp-SwiftUI
+//
+//  Created by loren on 2023/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("hi again")
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
