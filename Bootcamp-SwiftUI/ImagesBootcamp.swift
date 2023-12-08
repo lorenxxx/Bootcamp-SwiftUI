d6b1b632d584b09ffa4473aa66604b861ac21156
lorenliang <lorenliang@tencent.com>
2023-12-08 15:47:14 +0800

    feat: Images

---
+++ Bootcamp-SwiftUI/ImagesBootcamp.swift
@@ -0,0 +1,29 @@
+//
+//  Images.swift
+//  Bootcamp-SwiftUI
+//
+//  Created by lorenliang on 2023/12/8.
+//
+
+import SwiftUI
+
+struct Images: View {
+    var body: some View {
+        Image("lorenliang")
+            //.renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+            .resizable()
+            //.aspectRatio(contentMode: .fit)
+            .scaledToFit()
+            .frame(width: 300, height: 300)
+            //.cornerRadius(15)
+            .clipShape(
+                Circle()
+                //RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                //Ellipse()
+            )
+    }
+}
+
+#Preview {
+    Images()
+}
