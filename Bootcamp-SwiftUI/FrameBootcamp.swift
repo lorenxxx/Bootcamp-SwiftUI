fdbbdd7a0648a9a47751e9d3cab8e7b3f76e0098
lorenliang <lorenliang@tencent.com>
2023-12-08 16:05:15 +0800

    feat: Frame

---
+++ Bootcamp-SwiftUI/FrameBootcamp.swift
@@ -0,0 +1,23 @@
+//
+//  FrameBootcamp.swift
+//  Bootcamp-SwiftUI
+//
+//  Created by lorenliang on 2023/12/8.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .background(Color.green)
+            //.frame(width: 300, height: 300, alignment: .leading)
+            //.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            //.background(Color.red)
+            
+    }
+}
+
+#Preview {
+    FrameBootcamp()
+}
