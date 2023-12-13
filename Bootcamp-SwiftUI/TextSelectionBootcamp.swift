0a2eb3af34d8101eda4e894c0148cff744db785f
Loren <liangliangpro@outlook.com>
2023-12-14 00:50:17 +0800

    feat: Text Selection

---
+++ Bootcamp-SwiftUI/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  Bootcamp-SwiftUI
+//
+//  Created by loren on 2023/12/14.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
