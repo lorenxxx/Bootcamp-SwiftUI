8d895690f2a1e52d841d62456a7364bc6b6df8ae
lorenliang <lorenliang@tencent.com>
2023-12-14 18:31:42 +0800

    feat: ResizableSheet section 1

---
+++ Bootcamp-SwiftUI/ResizableSheet.swift
@@ -0,0 +1,43 @@
+//
+//  ResizableSheet.swift
+//  Bootcamp-SwiftUI
+//
+//  Created by lorenliang on 2023/12/14.
+//
+
+import SwiftUI
+
+struct ResizableSheet: View {
+    
+    @State private var showSheet: Bool = false
+    
+    var body: some View {
+        Button("Click Me") {
+            showSheet.toggle()
+        }
+        .sheet(isPresented: $showSheet, content: {
+            MySheetView()
+                //.presentationDetents([.medium])
+                .presentationDetents([.medium, .large])
+                //.presentationDetents([.fraction(0.33)])
+                //.presentationDragIndicator(.hidden)
+                //.interactiveDismissDisabled()
+        })
+//        .onAppear(perform: {
+//            showSheet = true
+//        })
+    }
+}
+
+struct MySheetView: View {
+    var body: some View {
+        ZStack {
+            Color.pink.ignoresSafeArea()
+            Text("Welcome")
+        }
+    }
+}
+
+#Preview {
+    ResizableSheet()
+}
